fd9bda16eb32536638222a5b59c32cec26b915b2
ivantxo <ivan.ramirezf@gmail.com>
2020-07-22 19:18:15 +1000

    Conditiional rendering
    - Added states for loading and error
    - Added conditional rendering

--- src/App.js
+++ src/App.js
@@ -50,11 +50,19 @@ function App() {
 
   const [stories, setStories] = React.useState([]);
 
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const [isError, setIsError] = React.useState(false);
+
   React.useEffect(() => {
-    getAsyncStories().then(result => {
-      setStories(result.data.stories);
-    });
-  });
+    setIsLoading(true);
+    getAsyncStories()
+      .then(result => {
+        setStories(result.data.stories);
+        setIsLoading(false);
+      })
+      .catch(() => setIsError(true));
+  }, []);
 
   const handleSearch = event => {
     setSearchTerm(event.target.value);
@@ -85,10 +93,18 @@ function App() {
         <strong>Search:</strong>
       </InputWithLabel>
       <hr />
-      <List
-        list={searchedStories}
-        onRemoveItem={handleRemoveStory}
-      />
+      {isError ? (
+        <p>Something went wrong...</p>
+      ) : (
+        isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <List
+            list={searchedStories}
+            onRemoveItem={handleRemoveStory}
+          />
+        )
+      )}
     </div>
   );
 }
