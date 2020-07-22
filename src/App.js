065d9fecdfdeb640c72bf9b1920e43aa6e9a0e0c
ivantxo <ivan.ramirezf@gmail.com>
2020-07-22 21:35:38 +1000

    Data Fetching 1
    - Added API endpoint
    - Using browser native API fetch
    - Updated fetch payload

--- src/App.js
+++ src/App.js
@@ -1,43 +1,6 @@
 import React from 'react';
 
-const initialStories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-  {
-    title: 'PHP',
-    url: 'https://php.net',
-    author: 'Rasmus Lerdorf',
-    num_comments: 10,
-    points: 10,
-    objectID: 2,
-  },
-];
-
-const getAsyncStories = () =>
-  new Promise(resolve =>
-      setTimeout(
-        () => resolve({ data: { stories: initialStories}}),
-        2000
-      )
-  );
-
-// Generate an error when getting stories
-// const getAsyncStories = () =>
-//   new Promise((resolve, reject) => setTimeout(reject, 2000));
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const useSemiPersistanceState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -96,11 +59,12 @@ function App() {
 
   React.useEffect(() => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}react`)
+      .then(response => response.json())
       .then(result => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.stories,
+          payload: result.hits,
         });
       })
       .catch(() => 
